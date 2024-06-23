29fb4afe772e1053c8cdb89eb3f85d43ceea2bb5
raqmarangoni <rmarangonisan@gmail.com>
2024-06-23 18:01:50 -0300

    feat: properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import {Post} from './Post'
+import { Post } from './Post'
 
 export function App() {
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post
+        author="Raquel Rodrigues"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores eveniet? Deserunt iure ab natus magnam laborum. Dolorem harum ullam vel quia ipsa, incidunt commodi ipsum, dolore maiores eveniet repudiandae."
+      />
+      <Post
+        author="Gabriel Rodrigues"
+        content="Novo post"
+      />
     </div>
   )
 }
