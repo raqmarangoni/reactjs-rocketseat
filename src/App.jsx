9f9e56812fc8d983e4642c5843c394b3e1beb623
raqmarangoni <rmarangonisan@gmail.com>
2024-06-24 21:09:03 -0300

    feat: application structure with css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header>
+        
+      </Header>
+
       <Post
         author="Raquel Rodrigues"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores eveniet? Deserunt iure ab natus magnam laborum. Dolorem harum ullam vel quia ipsa, incidunt commodi ipsum, dolore maiores eveniet repudiandae."
