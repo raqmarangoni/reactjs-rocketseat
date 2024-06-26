e4826e850b4b23827a47ca2dc78d21b2ecc970ed
raqmarangoni <rmarangonisan@gmail.com>
2024-06-25 23:13:40 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { Header } from './components/Header'
-import { Post } from './Post'
+import { Header } from './components/Header';
+import { Post } from './Post';
+import styles from './App.module.css';
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
-      <Header>
-        
-      </Header>
-      <Post
-        author="Raquel Rodrigues"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores eveniet? Deserunt iure ab natus magnam laborum. Dolorem harum ullam vel quia ipsa, incidunt commodi ipsum, dolore maiores eveniet repudiandae."
-      />
-      <Post
-        author="Gabriel Rodrigues"
-        content="Novo post"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Raquel Rodrigues"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores eveniet? Deserunt iure ab natus magnam laborum. Dolorem harum ullam vel quia ipsa, incidunt commodi ipsum, dolore maiores eveniet repudiandae."
+          />
+          <Post
+            author="Gabriel Rodrigues"
+            content="Novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
