14bd7561978fee0dc929322733b38ce26cc4ad74
raqmarangoni <rmarangonisan@gmail.com>
2024-07-05 22:34:54 -0300

    feat: Post component styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,10 +14,14 @@ export function Post() {
         <time title="01 de Julho às 08:13h" dateTime="2024-07-01 08:13:30">Publicado há 1h</time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p> 
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p> 
-        <p>👉<a href="">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
