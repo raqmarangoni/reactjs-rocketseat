643510c800074279124f1551b83d35d5c183acfa
raqmarangoni <rmarangonisan@gmail.com>
2024-07-08 17:31:12 -0300

    feat: comment styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -13,7 +13,7 @@ export function Comment() {
               <time title="01 de Julho às 08:13h" dateTime="2024-07-01 08:13:30">Cerca de 1h atrás</time>
             </div>
             <button title="Deletar comentário">
-            <Trash size={20}/>
+            <Trash size={24}/>
             </button>
           </header>
 
